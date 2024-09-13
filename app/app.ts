3636db484586cf54c896a8d58387d3d4aa07fd36
stevethomas <steve@codinglabs.io>
2024-09-13 11:55:23 +1000

    move app files

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue"
+import "@/presets/slate.css"
 import "@app/style.css"
 import App from "./App.vue"
 import router from "./router"
 
-createApp(App).use(router).mount("#app")
+createApp(App)
+  .use(router)
+  .mount("#app")
