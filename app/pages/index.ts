2a2a117b007c781edbe9cfc15bb12a416c05e67f
stevethomas <steve@codinglabs.com.au>
2024-09-14 10:30:07 +1000

    add button

---
+++ app/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Index } from './Index.vue'
+
+export { default as DropdownMenu } from './DropdownMenu.vue'
+export { default as Notification } from './Notification.vue'
+export { default as ShadcnComponent } from './ShadcnComponent.vue'
+export { default as Toast } from './Toast.vue'
--- app/router/index.ts
+++ app/router/index.ts
@@ -1,17 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-import Home from "@app/Home.vue"
-import ShadcnComponent from "@app/ShadcnComponent.vue"
-import Notification from "@app/pages/Notification.vue"
-import Toast from "@app/pages/Toast.vue"
+import { Index, DropdownMenu, Notification, ShadcnComponent, Toast } from "@app/pages"
 
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: Index },
   {
     path: "/components",
     name: "Components",
     children: [
-      { name: "Dropdown Menu", path: "/components/dropdown-menu", component: ShadcnComponent },
+      { name: "Button", path: "/components/button", component: ShadcnComponent },
+      { name: "Dropdown Menu", path: "/components/dropdown-menu", component: DropdownMenu },
       { name: "Notification", path: "/components/notification", component: Notification },
       { name: "Sheet", path: "/components/sheet", component: ShadcnComponent },
       { name: "Switch", path: "/components/switch", component: ShadcnComponent },
