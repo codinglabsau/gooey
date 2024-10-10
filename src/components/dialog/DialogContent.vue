09cc355cd0e1a9b79f45f73081dc1b4e3d1786d6
Seth Sharp <sesharp@outlook.com>
2024-10-11 08:58:25 +1000

    emit close on click

--- src/components/dialog/DialogContent.vue
+++ src/components/dialog/DialogContent.vue
@@ -13,7 +13,11 @@ import { Cross2Icon } from "@radix-icons/vue"
 import { cn } from "@/utils/shadcdn"
 
 const props = defineProps<DialogContentProps & { class?: HTMLAttributes["class"] }>()
-const emits = defineEmits<DialogContentEmits>()
+const emits = defineEmits<
+  DialogContentEmits & {
+    close: [event: Event]
+  }
+>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
@@ -43,8 +47,9 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 
       <DialogClose
         class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+        @click="emits('close', $event)"
       >
-        <Cross2Icon class="h-4 w-4" />
+        <Cross2Icon class="size-4" />
 
         <span class="sr-only">Close</span>
       </DialogClose>
