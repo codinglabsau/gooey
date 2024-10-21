0bc89b64650ed5b4e6e944848d2322561250f147
stevethomas <steve@codinglabs.com.au>
2024-10-21 17:34:38 +1000

    fine-tune header classes

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-accent bg-background/80 px-4 shadow-sm backdrop-blur sm:gap-x-6 sm:px-6 lg:px-8"
+    class="sticky top-0 z-50 flex h-16 shrink-0 items-center gap-x-4 bg-background/60 px-4 backdrop-blur sm:gap-x-6 sm:px-6 lg:px-8"
   >
     <slot />
   </header>
