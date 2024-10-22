ab440c67e86aedf20c8b9df45bfce5649a1cfd25
TJeffery <tjefferyj@gmail.com>
2024-10-22 15:22:07 +1000

    cleaning up

--- app/pages/components/Accordion.vue
+++ app/pages/components/Accordion.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-
 import { Accordion } from '@/components/accordion'
-import { Header, Label } from "@"
 
 const content = {
   first: { title: 'First', content: 'First content' },
@@ -20,7 +18,7 @@ const content = {
       <h2 class="text-2xl">Accordion with Custom Content and Title</h2>
       <Accordion :content="content">
         <template #second.title="{ item }">
-          <Label> Custom </Label>
+          <h3 class="text-2xl"> Custom title for {{ item.title }} </h3>
         </template>
         <template #second.content="{ item }">
           <img src="/logo.svg" alt="Coding Labs UI" class="w-52 mx-auto" />
--- src/components/accordion/Accordion.vue
+++ src/components/accordion/Accordion.vue
@@ -7,7 +7,6 @@ import {
 } from 'radix-vue'
 import { AccordionContent, AccordionItem, AccordionTrigger } from '@/components/accordion'
 
-
 interface AccordionItem {
   title: string
   content: string
@@ -28,13 +27,17 @@ const forwarded = useForwardPropsEmits(props, emits)
 </script>
 
 <template>
-      <AccordionRoot v-bind="forwarded"  >
-        <AccordionItem v-for="(item, index) in content" :value="'item-' + index" :key="index">
+      <AccordionRoot v-bind="forwarded">
+        <AccordionItem v-for="(item, key) in content" :value="'item-' + key" :key="key">
           <AccordionTrigger>
-            <slot :name="index + '.title'" :item="item"> {{ item.title }}</slot>
+            <slot :name="key + '.title'" :item="item">
+              {{ item.title }}
+            </slot>
           </AccordionTrigger>
           <AccordionContent>
-            <slot :name="index + '.content'" :item="item"> {{ item.content }}</slot>
+            <slot :name="key + '.content'" :item="item">
+              {{ item.content }}
+            </slot>
           </AccordionContent>
         </AccordionItem>
     </AccordionRoot>
