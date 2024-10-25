4213102c7c85c6f3e242aa26db23c6ed8b62ed7a
TJeffery <tjefferyj@gmail.com>
2024-10-25 12:21:05 +1000

    Reverted shadcn tooltip to original init, replaced use with Tip wrapping component

--- src/components/tooltip/index.ts
+++ src/components/tooltip/index.ts
@@ -1,3 +1,4 @@
+export { default as Tip } from "./Tip.vue"
 export { default as Tooltip } from "./Tooltip.vue"
 export { default as TooltipContent } from "./TooltipContent.vue"
 export { default as TooltipProvider } from "./TooltipProvider.vue"
