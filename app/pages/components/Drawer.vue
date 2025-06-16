f3d03e5ba7776d697eb3960c6dd5c77078c26810
Victoria Suzuki <53244381+vrodriguero@users.noreply.github.com>
2025-06-16 11:41:57 +1000

    Vickie/add drawer (#14)
    * add drawer component to gooey
    
    * add tests

---
+++ app/pages/components/Drawer.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { Button } from "@/components/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTrigger,
+  DrawerContent,
+  DrawerTitle
+} from "@/components/drawer"
+import { Minus, Plus } from "lucide-vue-next"
+
+const goal = ref(5)
+</script>
+
+<template>
+  <Drawer>
+    <DrawerTrigger as-child>
+      <Button variant="outline">
+        Open Drawer
+      </Button>
+    </DrawerTrigger>
+    <DrawerContent>
+      <div class="mx-auto w-full max-w-sm">
+        <DrawerHeader>
+          <DrawerTitle>Backlog tasks</DrawerTitle>
+          <DrawerDescription>Set your daily goal.</DrawerDescription>
+        </DrawerHeader>
+        <div class="p-4 pb-0">
+          <div class="flex items-center justify-center space-x-2">
+            <Button
+              variant="outline"
+              size="icon"
+              class="h-8 w-8 shrink-0 rounded-full"
+              :disabled="goal <= 0"
+              @click="goal -= 1"
+            >
+              <Minus class="h-4 w-4" />
+              <span class="sr-only">Decrease</span>
+            </Button>
+            <div class="flex-1 text-center">
+              <div class="text-7xl font-bold tracking-tighter">
+                {{ goal }}
+              </div>
+              <div class="text-[0.70rem] uppercase text-muted-foreground">
+                Issues per day
+              </div>
+            </div>
+            <Button
+              variant="outline"
+              size="icon"
+              class="h-8 w-8 shrink-0 rounded-full"
+              :disabled="goal >= 10"
+              @click="goal += 1"
+            >
+              <Plus class="h-4 w-4" />
+              <span class="sr-only">Increase</span>
+            </Button>
+          </div>
+        </div>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose as-child>
+            <Button variant="outline">
+              Cancel
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </div>
+    </DrawerContent>
+  </Drawer>
+</template>
