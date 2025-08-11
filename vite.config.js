c27e69216c10c2f5c6bea736253b5a5af50a4197
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2025-08-11 14:28:55 +1000

    revert vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,44 @@
 import path from "node:path"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import Markdown from "unplugin-vue-markdown/vite"
 import { viteStaticCopy } from "vite-plugin-static-copy"
+import Markdown from 'unplugin-vue-markdown/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const isBuild = command === "build"
-
-  return {
-    build: {
-      lib: {
-        entry: path.resolve(__dirname, "./src/index.ts"),
-        name: "gooey",
-        fileName: "gooey"
-      },
-      rollupOptions: {
-        external: [
-          "vue",
-          "@inertiajs/vue3"
-        ]
-      },
-      copyPublicDir: false,
-      emptyOutDir: false
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      name: "gooey",
+      fileName: "gooey"
     },
-    plugins: [
-      vue({
-        include: [/\.vue$/, /\.md$/],
-      }),
-      Markdown({ /* options */ }),
-      viteStaticCopy({
-        targets: [
-          {
-            src: "src/presets/slate.css",
-            dest: "presets"
-          }
-        ]
-      })
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "@app": path.resolve(__dirname, "./app")
-      }
+    rollupOptions: {
+      external: [
+        "vue",
+        "@inertiajs/vue3"
+      ]
+    },
+    copyPublicDir: false,
+    emptyOutDir: false
+  },
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown({ /* options */ }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/presets/slate.css",
+          dest: "presets"
+        }
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./app")
     }
   }
 })
