e84a5a6a0749a434296b9b1e40368cde305e6738
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2025-09-19 11:31:56 +1000

    add success/warning colour configs

--- app/pages/components/Badge.vue
+++ app/pages/components/Badge.vue
@@ -3,5 +3,17 @@ import { Badge } from "@/components/badge"
 </script>
 
 <template>
-  <Badge data-cy="badge"> Badge </Badge>
+  <div class="flex gap-2">
+    <Badge data-cy="badge"> Badge </Badge>
+
+    <Badge data-cy="badge" variant="secondary"> Secondary Badge </Badge>
+
+    <Badge data-cy="badge" variant="destructive"> Destructive Badge </Badge>
+
+    <Badge data-cy="badge" variant="outline"> Outline Badge </Badge>
+
+    <Badge data-cy="badge" variant="success"> Success Badge </Badge>
+
+    <Badge data-cy="badge" variant="warning"> Warning Badge </Badge>
+  </div>
 </template>
