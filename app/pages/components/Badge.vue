cfa14d41bb0bf7adcbe013cc6ae2550822a92480
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2025-09-19 11:48:16 +1000

    Additional Badge Variants + Introduce `success` and `warning` Colour Configs (#22)
    * add success/warning colour configs
    
    * wip
    
    * formatting

--- app/pages/components/Badge.vue
+++ app/pages/components/Badge.vue
@@ -3,5 +3,17 @@ import { Badge } from "@/components/badge"
 </script>
 
 <template>
-  <Badge data-cy="badge"> Badge </Badge>
+  <div class="flex gap-2">
+    <Badge data-cy="badge"> Badge </Badge>
+
+    <Badge data-cy="badge-secondary" variant="secondary"> Secondary Badge </Badge>
+
+    <Badge data-cy="badge-destructive" variant="destructive"> Destructive Badge </Badge>
+
+    <Badge data-cy="badge-outline" variant="outline"> Outline Badge </Badge>
+
+    <Badge data-cy="badge-success" variant="success"> Success Badge </Badge>
+
+    <Badge data-cy="badge-warning" variant="warning"> Warning Badge </Badge>
+  </div>
 </template>
