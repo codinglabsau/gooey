7db0c9e2583bd2a78bb96e9b553d4ef7e5417f4d
stevethomas <steve@codinglabs.io>
2026-01-19 18:54:17 +1000

    wip

--- src/components/layout/TwoColumnLayoutSidebarDesktop.vue
+++ src/components/layout/TwoColumnLayoutSidebarDesktop.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="hidden px-6 py-6 lg:fixed lg:inset-y-0 lg:top-16 lg:z-50 lg:flex lg:w-72 lg:flex-col"
-  >
+  <div class="hidden px-6 py-6 lg:fixed lg:inset-y-0 lg:top-16 lg:z-50 lg:flex lg:w-72 lg:flex-col">
     <div class="gap-y-5 overflow-y-auto">
       <slot />
     </div>
