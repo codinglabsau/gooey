c79eeb0bff09ff93a9d8bcd4e5e421e98fc7e6de
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2026-02-04 15:36:19 +1030

    prepare release (2.1.0)

--- dist/types/components/context-menu/ContextMenuRadioGroup.vue.d.ts
+++ dist/types/components/context-menu/ContextMenuRadioGroup.vue.d.ts
@@ -9,9 +9,9 @@ declare function __VLS_template(): {
 };
 type __VLS_TemplateResult = ReturnType<typeof __VLS_template>;
 declare const __VLS_component: import('vue').DefineComponent<ContextMenuRadioGroupProps, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
-    "update:modelValue": (payload: string) => any;
+    "update:modelValue": (payload: import('reka-ui').AcceptableValue) => any;
 }, string, import('vue').PublicProps, Readonly<ContextMenuRadioGroupProps> & Readonly<{
-    "onUpdate:modelValue"?: ((payload: string) => any) | undefined;
+    "onUpdate:modelValue"?: ((payload: import('reka-ui').AcceptableValue) => any) | undefined;
 }>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, any>;
 declare const _default: __VLS_WithTemplateSlots<typeof __VLS_component, __VLS_TemplateResult["slots"]>;
 export default _default;
--- dist/types/components/flasher/use-flasher.d.ts
+++ dist/types/components/flasher/use-flasher.d.ts
@@ -1,9 +1,11 @@
+import { ExternalToast } from 'vue-sonner';
 export type ErrorBag = Record<string, string>;
 export type ObjectFormat = "key" | "value" | "both";
 declare function useFlasher(): {
-    info: (message: string) => void;
-    success: (message: string) => void;
-    warning: (message: string) => void;
-    error: (errors: ErrorBag, objectFormat?: ObjectFormat) => void;
+    info: (message: string, data?: ExternalToast) => void;
+    success: (message: string, data?: ExternalToast) => void;
+    warning: (message: string, data?: ExternalToast) => void;
+    error: (errors: ErrorBag, objectFormat?: ObjectFormat, data?: ExternalToast) => void;
+    flash: (heading: string, data?: ExternalToast) => void;
 };
 export { useFlasher };
